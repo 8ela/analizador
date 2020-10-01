2ee2168bd05eabf05500551f51231ce2747609cb
santiagoSSAA <s.sanchez123@utp.edu.co>
2020-09-30 20:04:28 -0500

    gt bug solved

--- input.pas
+++ input.pas
@@ -1,23 +1,14 @@
-(*****************************************************************************
- * A simple bubble sort program.  Reads integers, one per line, and prints   *
- * them out in sorted order.  Blows up if there are more than 49.            *
- *****************************************************************************)
 PROGRAM Sort(input, output);
     CONST
-        (* Max array size. *)
         MaxElts = 50;
     TYPE
-        (* Type of the element array. *)
         IntArrType = ARRAY [1..MaxElts] OF Integer;
 
     VAR
-        (* Indexes, exchange temp, array size. *)
         i, j, tmp, size: integer;
-
-        (* Array of ints *)
+        
         arr: IntArrType;
 
-    (* Read in the integers. *)
     PROCEDURE ReadArr(VAR size: Integer; VAR a: IntArrType);
         BEGIN
             size := 1;
@@ -29,10 +20,8 @@ PROGRAM Sort(input, output);
         END;
 
     BEGIN
-        (* Read *)
         ReadArr(size, arr);
 
-        (* Sort using bubble sort. *)
         FOR i := size - 1 DOWNTO 1 DO
             FOR j := 1 TO i DO
                 IF arr[j] > arr[j + 1] THEN BEGIN
@@ -41,7 +30,6 @@ PROGRAM Sort(input, output);
                     arr[j + 1] := tmp;
                 END;
 
-        (* Print. *)
         FOR i := 1 TO size DO
             writeln(arr[i])
     END.
